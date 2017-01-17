6be521b0141cca91df926a9c1f12d1dee45f43f2
ownport <ownport@gmail.com>
2017-01-17 23:42:24 +0200

    [+] use Ansiblite() as main point of entrance

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,11 @@
 
+import sys
+
+# Cleaner removes from sys.path any external libs to avoid potential
+# conflicts with existing system libraries
+from ansiblite.utils.pyenv import Cleaner
+sys.path = Cleaner.syspath()
+
 from ansiblite import Ansiblite
+
 Ansiblite()
