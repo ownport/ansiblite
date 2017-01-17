c9deba287ff4fe232547deb6b9275a4ae6f4f0ae
ownport <ownport@gmail.com>
2017-01-17 23:30:16 +0200

    [+] test command

---
+++ src/ansiblite/test/module.py
@@ -0,0 +1,38 @@
+from __future__ import (absolute_import, division, print_function)
+
+import sys
+import json
+
+
+class Stdin():
+
+    def __init__(self, args):
+
+        self._args = args
+
+    def read(self):
+        return json.dumps(dict(ANSIBLE_MODULE_ARGS={}))
+
+
+def run_test(module_name, args):
+
+    # TODO wrap stdin & stdout
+    # from pprint import pprint
+
+    sys.stdin = Stdin(args)
+    try:
+        module = __import__(
+            'ansiblite.modules.{}'.format(module_name),
+            globals(), locals(), ['main'])
+        module.main()
+    except ImportError, err:
+        print(
+            "[ERROR] Cannot import module 'ansiblite.modules.{}'. Error: {}".format(
+                args.module_path, err.message)
+        )
+
+        import traceback
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_tb(exc_traceback)
+        sys.exit(1)
+    # print stdout.get()
